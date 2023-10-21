e026f3655f8c036302062c3c8c40bdc0a42ebc8d
Vivek Kumar Chaudhary <chaudharv101@gmail.com>
2023-10-21 22:49:59 +0530

    test cases, and some db config

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+def add(x: int, y: int) -> int:
+    """Add x and y and return the result."""
+    return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    """Subtract y from x and return the result"""
+    return x - y
